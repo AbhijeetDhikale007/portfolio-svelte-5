b23fe7f8eacdb269a7ca40ac9e479b8d5f3eff99
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2024-12-25 17:02:29 +0000

    Icons Added

--- src/components/Icons/Icons.ts
+++ src/components/Icons/Icons.ts
@@ -12,5 +12,101 @@ export let icons = {
     Bootstrap: {
         viewBox: '0 0 256 256',
         svg: `<g fill="none"><rect width="256" height="256" fill="url(#skillIconsBootstrap0)" rx="60"/><g filter="url(#skillIconsBootstrap2)"><path fill="url(#skillIconsBootstrap1)" d="M131.97 196.157c29.676 0 47.559-14.531 47.559-38.497c0-18.117-12.759-31.232-31.706-33.309v-.754c13.92-2.265 24.843-15.192 24.843-29.628c0-20.57-16.239-33.969-40.986-33.969H76v136.157zM97.653 77.267h28.807c15.66 0 24.553 6.983 24.553 19.627c0 13.493-10.343 21.041-29.096 21.041H97.653zm0 101.623v-44.819h28.613c20.494 0 31.127 7.548 31.127 22.268c0 14.719-10.343 22.551-29.87 22.551z"/><path stroke="#fff" stroke-width="2" d="M131.97 196.157c29.676 0 47.559-14.531 47.559-38.497c0-18.117-12.759-31.232-31.706-33.309v-.754c13.92-2.265 24.843-15.192 24.843-29.628c0-20.57-16.239-33.969-40.986-33.969H76v136.157zM97.653 77.267h28.807c15.66 0 24.553 6.983 24.553 19.627c0 13.493-10.343 21.041-29.096 21.041H97.653zm0 101.623v-44.819h28.613c20.494 0 31.127 7.548 31.127 22.268c0 14.719-10.343 22.551-29.87 22.551z"/></g><defs><linearGradient id="skillIconsBootstrap0" x1="0" x2="256" y1="0" y2="256" gradientUnits="userSpaceOnUse"><stop stop-color="#9013fe"/><stop offset="1" stop-color="#6b11f4"/></linearGradient><linearGradient id="skillIconsBootstrap1" x1="85.793" x2="148.541" y1="68.962" y2="175.084" gradientUnits="userSpaceOnUse"><stop stop-color="#fff"/><stop offset="1" stop-color="#f1e5fc"/></linearGradient><filter id="skillIconsBootstrap2" width="137.529" height="170.157" x="59" y="47" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="4"/><feGaussianBlur stdDeviation="8"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow_158_100"/><feBlend in="SourceGraphic" in2="effect1_dropShadow_158_100" result="shape"/></filter></defs></g>`
-    }
+    },
+
+    CPlus: {
+        viewBox: '0 0 256 256',
+        svg: `<g fill="none"><rect width="256" height="256" fill="#00599c" rx="60"/><path fill="#fff" d="M110.759 210.517C65.125 210.517 28 173.392 28 127.759C28 82.125 65.125 45 110.759 45c29.445 0 56.908 15.846 71.668 41.353l-35.816 20.726c-7.387-12.768-21.126-20.7-35.852-20.7c-22.817 0-41.38 18.563-41.38 41.38c0 22.816 18.563 41.379 41.38 41.379c14.727 0 28.466-7.932 35.854-20.702l35.816 20.725c-14.76 25.51-42.223 41.356-71.67 41.356"/><path fill="#fff" d="M193.517 123.161h-9.196v-9.196h-9.194v9.196h-9.196v9.195h9.196v9.196h9.194v-9.196h9.196zm34.483 0h-9.196v-9.196h-9.194v9.196h-9.196v9.195h9.196v9.196h9.194v-9.196H228z"/></g>`
+    },
+
+    Certificate: {
+        viewBox: '0 0 24 24',
+        svg: `<g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M12 15a3 3 0 1 0 6 0a3 3 0 1 0-6 0"/><path d="M13 17.5V22l2-1.5l2 1.5v-4.5"/><path d="M10 19H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-1 1.73M6 9h12M6 12h3m-3 3h2"/></g>`
+    },
+
+    Circle: {
+        viewBox: '0 0 24 24',
+        svg: `<path fill="#fff" d="M20.788 3.832c-.101-.105-.197-.213-.301-.317q-.155-.153-.32-.302A11.9 11.9 0 0 0 12 0a11.93 11.93 0 0 0-8.486 3.514C-1.062 8.09-1.16 15.47 3.213 20.168q.147.162.3.32c.104.103.21.2.317.3A11.92 11.92 0 0 0 12 24c3.206 0 6.22-1.247 8.487-3.512c4.576-4.576 4.673-11.956.301-16.656m-16.655.301A11.06 11.06 0 0 1 12 .874c2.825 0 5.49 1.048 7.55 2.958l-1.001 1.002A9.65 9.65 0 0 0 12 2.292a9.64 9.64 0 0 0-6.865 2.844A9.64 9.64 0 0 0 2.292 12c0 2.448.9 4.753 2.542 6.549L3.831 19.55C-.201 15.191-.101 8.367 4.133 4.133m13.798 1.318v.002l-1.015 1.014A7.35 7.35 0 0 0 12 4.589A7.36 7.36 0 0 0 6.761 6.76A7.36 7.36 0 0 0 4.589 12a7.34 7.34 0 0 0 1.877 4.913l-1.014 1.016A8.77 8.77 0 0 1 3.167 12a8.77 8.77 0 0 1 2.588-6.245A8.77 8.77 0 0 1 12 3.167c2.213 0 4.301.809 5.931 2.284M18.537 12c0 1.745-.681 3.387-1.916 4.622S13.746 18.538 12 18.538a6.5 6.5 0 0 1-4.296-1.621l-.001-.004c-.11-.094-.22-.188-.324-.291a6 6 0 0 1-.293-.326A6.47 6.47 0 0 1 5.466 12c0-1.746.679-3.387 1.914-4.621A6.5 6.5 0 0 1 12 5.465c1.599 0 3.105.576 4.295 1.62c.111.096.224.19.326.295q.154.157.295.324A6.48 6.48 0 0 1 18.537 12M7.084 17.534h.001A7.35 7.35 0 0 0 12 19.413a7.35 7.35 0 0 0 5.239-2.174A7.35 7.35 0 0 0 19.412 12a7.36 7.36 0 0 0-1.876-4.916l1.013-1.012A8.78 8.78 0 0 1 20.834 12a8.77 8.77 0 0 1-2.589 6.246A8.76 8.76 0 0 1 12 20.834a8.78 8.78 0 0 1-5.93-2.285zm12.783 2.333A11.05 11.05 0 0 1 12 23.125a11.04 11.04 0 0 1-7.551-2.957l1.004-1.001a9.64 9.64 0 0 0 6.549 2.542a9.64 9.64 0 0 0 6.865-2.846A9.64 9.64 0 0 0 21.71 12a9.64 9.64 0 0 0-2.543-6.548l1.001-1.002c4.031 4.359 3.935 11.182-.301 15.417"/>`
+    },
+
+    CLang: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Close: {
+        viewBox: '0 0 24 24',
+        svg: `<g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M5 12H19"><animate fill="freeze" attributeName="d" dur="1.2s" values="M5 12H19;M12 12H12"/><set fill="freeze" attributeName="opacity" begin="1.2s" to="0"/></path><path d="M5 5L19 5M5 19L19 19" opacity="0"><animate fill="freeze" attributeName="d" begin="0.6s" dur="1.2s" values="M5 5L19 5M5 19L19 19;M5 5L19 19M5 19L19 5"/><set fill="freeze" attributeName="opacity" begin="0.6s" to="1"/></path></g>`
+    },
+
+    CSS: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Dart: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Date: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Eclipse: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Flutter: {
+        viewBox: '',
+        svg: ``
+    },
+
+    GitHub: {
+        viewBox: '',
+        svg: ``
+    },
+
+    HTML: {
+        viewBox: '',
+        svg: ``
+    },
+
+    IntelliJ: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Java: {
+        viewBox: '',
+        svg: ``
+    },
+
+    JavaScript: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Link: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Location: {
+        viewBox: '',
+        svg: ``
+    },
+
+    Menu: {
+        viewBox: '',
+        svg: ``
+    },
+
+    MySQL: {
+        viewBox: '',
+        svg: ``
+    },
+
 } as const;
