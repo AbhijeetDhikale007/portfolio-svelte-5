752704458badbb44eec53ae0a9830394906ea54c
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2024-12-26 16:14:41 +0000

    Ico Added

--- src/components/+components.ts
+++ src/components/+components.ts
@@ -1,3 +1,4 @@
+import Ico from './Icons/Ico.svelte'
 import Icons from './Home/Icons.svelte'
 import Carousel from './Home/Carousel.svelte'
 import Heading from "./Heading.svelte";
@@ -7,4 +8,4 @@ import CardDiv from "./Skills/CardDiv.svelte";
 import { Card1, Card2, Card3, Card4, Card5, Card6, Card7 } from "./Skills/Cards";
 import Typewriter from "./Typewriter.svelte";
 
-export { Icons, Carousel, Heading, EducationCard, CardProject, CardDiv, Card1, Card2, Card3, Card4, Card5, Card6, Card7, Typewriter}
+export { Ico, Icons, Carousel, Heading, EducationCard, CardProject, CardDiv, Card1, Card2, Card3, Card4, Card5, Card6, Card7, Typewriter}
