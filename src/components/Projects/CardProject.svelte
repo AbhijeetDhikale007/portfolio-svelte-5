4ad1649abac60ae4b0beb814e7a055daa37fc68c
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2024-12-27 16:44:20 +0000

    Ico Added

--- src/components/Projects/CardProject.svelte
+++ src/components/Projects/CardProject.svelte
@@ -1,6 +1,7 @@
 <script lang=ts>
     import { onMount } from "svelte"
     import { Project } from '../../data/Projects'
+    import { Ico } from '../+components'
     import VanillaTilt from "vanilla-tilt"
 
     let TiltCard: HTMLElement[] = [];
@@ -30,18 +31,22 @@
     <div class='CP1'>
         <h2>{project.Title}</h2>
         <div class='CP1Div'>
-            <a class='Tooltip' data-title='GitHub' href={project.urlGit} target='_blank'><img src="Icons/GitHub-white.svg" alt="Link"></a>
-            <a class='Tooltip' data-title='Deployment' href={project.urlDeploy} target='_blank'><img src="Icons/Link.svg" alt="Link"></a>
+            <a class='Tooltip' data-title='GitHub' href={project.urlGit} target='_blank'>
+                <Ico name='GitHubW' />
+            </a>
+            <a class='Tooltip' data-title='Deployment' href={project.urlDeploy} target='_blank'>
+                <Ico name='Link' />
+            </a>
         </div>
     </div>
     <hr>
     <div class='CP2'>
-        <img class='SVG' src="Icons/Project-Type.svg" alt="Icon">
+        <Ico name='Project' class='Ico' />
         <h4>{project.Type}</h4>
     </div>
     <hr>
     <div class="CP3">
-        <img class='SVG' src="Icons/Time.svg" alt="Duration">
+        <Ico name='Time' class='Ico' />
         <h4>{project.Duration}</h4>
     </div>
     <hr>
@@ -69,6 +74,16 @@
 {/each}
 
 <style lang='scss'>
+    .CardProject {
+        a {
+            @apply w-5 h-5 flex content-center;
+        }
+
+        :global(.Ico) {
+            @apply w-5 h-5;
+        }
+    }
+
     .SVG {
         @apply w-4.8 h-4.8;
     }
