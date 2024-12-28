73a3d554c111d12793e7a54737fb36095c18120b
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2024-12-28 16:55:56 +0000

    Types & Ico Implimented

--- src/data/Skills/Detail.ts
+++ src/data/Skills/Detail.ts
@@ -1,63 +1,20 @@
-// import { C, CPlus, TypeScript, JavaScript, HTML, CSS, SCSS, UnoCSS, TailwindCSS, Bootstrap, WordPress, Webflow, Svelte, React, Angular, Threejs, MySQL, Postman, VSCode, GitHub, ProjectIDX, Eclipse, IntelliJ } from "../SkillPages/PagesInfo"
-
-// const Skill1 = [
-//     { Page: C , name: 'C', class: 'SVG C', SVG: 'Icons/CLang.svg', Alt: 'C' },
-//     { Page: CPlus, name: 'C++', class: 'SVG CPlus', SVG: 'Icons/C++.svg', Alt: 'C++' },
-//     { Page: TypeScript, name: 'TypeScript', class: 'SVG TypeScript', SVG: 'Icons/TypeScript.svg', Alt: 'TypeScript' },
-//     { Page: JavaScript, name: 'JavaScript', class: 'SVG Javascript', SVG: 'Icons/Javascript.svg', Alt: 'JavaScript' }
-// ]
-
-// const Skill2 = [
-//     { Page: HTML, name: 'HTML 5', class: 'SVG HTML', SVG: 'Icons/HTML.svg', Alt: 'HTML' },
-//     { Page: CSS, name: 'CSS 3', class: 'SVG CSS', SVG: "Icons/CSS.svg", Alt: 'CSS' },
-//     { Page: SCSS, name: 'SCSS', class: 'SVG SCSS', SVG: "Icons/Sass.svg", Alt: 'Sass' },
-//     { Page: UnoCSS, name: 'UnoCSS', class: 'SVG UnoCSS', SVG: "Icons/Unocss.svg", Alt: 'UnoCSS' },
-//     { Page: TailwindCSS, name: 'Tailwind CSS', class: 'SVG Tailwind', SVG: "Icons/Tailwind.svg", Alt: 'Tailwind' },
-//     { Page: Bootstrap, name: 'Bootstrap', class: 'SVG Bootstrap', SVG: "Icons/Bootstrap.svg", Alt: 'Bootstrap' }
-// ]
-
-// const Skill3 = [
-//     { Page: WordPress, name: 'WordPress', class: 'SVG Wordpress', SVG: 'Icons/Wordpress.svg', Alt: 'WordPress' },
-//     { Page: Webflow, name: 'Webflow', class: 'SVG Webflow', SVG: 'Icons/Webflow.svg', Alt: 'Webflow' }
-// ]
-
-// const Skill4 = [
-//     { Page: Svelte, name: 'Svelte', class: 'SVG Svelte', SVG: "Icons/Svelte.svg", Alt: 'Svelte' },
-//     { Page: React, name: 'React', class: 'SVG React', SVG: "Icons/React.svg", Alt: 'React' },
-//     { Page: Angular, name: 'Angular', class: 'SVG Angular', SVG: "Icons/Angular.svg", Alt: 'Angular' },
-//     { Page: Threejs, name: 'Three js', class: 'SVG Threejs', SVG: "Icons/Threejs.svg", Alt: 'Three js' },
-// ]
-
-// const Skill5 = [
-//     { Page: MySQL, name: 'MySQL', class: 'SVG MySQL', SVG: "Icons/Mysql.svg", Alt: 'MySQL' }
-// ]
-
-// const Skill6 = [
-//     { Page: Postman, name: 'Postman', class: 'SVG Postman', SVG: "Icons/Postman.svg", Alt: 'Postman' }
-// ]
-
-// const Skill7 = [
-//     { Page: VSCode, name: 'VSCode', class: 'SVG VSCode', SVG: 'Icons/VSCode.svg', Alt: 'VS Code' },
-//     { Page: GitHub, name: 'GitHub', class: 'SVG GitHub', SVG: 'Icons/Github.svg', Alt: 'GitHub' },
-//     { Page: ProjectIDX, name: 'Project IDX', class: 'SVG ProjectIDX', SVG: 'Icons/Project-IDX.png', Alt: 'Project IDX' },
-//     { Page: Eclipse, name: 'Eclipse', class: 'SVG Eclipse', SVG: 'Icons/Eclipse.svg', Alt: 'Eclipse' },
-//     { Page: IntelliJ, name: 'IntelliJ', class: 'SVG InteliJ', SVG: 'Icons/Idea.svg', Alt: 'InteliJ Idea' },
-// ]
-
-// export { Skill1, Skill2, Skill3, Skill4, Skill5, Skill6, Skill7 }
-
-// -------------------------
-
 import { C, CPlus, TypeScript, JavaScript, HTML, CSS, SCSS, UnoCSS, TailwindCSS, Bootstrap, WordPress, Webflow, Svelte, React, Angular, Threejs, MySQL, Postman, VSCode, GitHub, ProjectIDX, Eclipse, IntelliJ } from "../SkillPages/PagesInfo"
 
-const Skill1 = [
+type Type = {
+    Page: any,
+    name: string,
+    class: string,
+    SVG: string
+}[]
+
+const Skill1: Type = [
     { Page: C , name: 'C', class: 'SVG C', SVG: 'CLang' },
     { Page: CPlus, name: 'C++', class: 'SVG CPlus', SVG: 'CPlus' },
     { Page: TypeScript, name: 'TypeScript', class: 'SVG TypeScript', SVG: 'TypeScript' },
     { Page: JavaScript, name: 'JavaScript', class: 'SVG Javascript', SVG: 'JavaScript' }
 ]
 
-const Skill2 = [
+const Skill2: Type = [
     { Page: HTML, name: 'HTML 5', class: 'SVG HTML', SVG: 'HTML' },
     { Page: CSS, name: 'CSS 3', class: 'SVG CSS', SVG: "CSS" },
     { Page: SCSS, name: 'SCSS', class: 'SVG SCSS', SVG: "Sass" },
@@ -66,32 +23,32 @@ const Skill2 = [
     { Page: Bootstrap, name: 'Bootstrap', class: 'SVG Bootstrap', SVG: "Bootstrap" }
 ]
 
-const Skill3 = [
-    { Page: WordPress, name: 'WordPress', class: 'SVG Wordpress', SVG: 'WordPress', Alt: 'WordPress' },
-    { Page: Webflow, name: 'Webflow', class: 'SVG Webflow', SVG: 'Webflow', Alt: 'Webflow' }
+const Skill3: Type = [
+    { Page: WordPress, name: 'WordPress', class: 'SVG Wordpress', SVG: 'WordPress' },
+    { Page: Webflow, name: 'Webflow', class: 'SVG Webflow', SVG: 'Webflow' }
 ]
 
-const Skill4 = [
-    { Page: Svelte, name: 'Svelte', class: 'SVG Svelte', SVG: "Svelte", Alt: 'Svelte' },
-    { Page: React, name: 'React', class: 'SVG React', SVG: "React", Alt: 'React' },
-    { Page: Angular, name: 'Angular', class: 'SVG Angular', SVG: "Angular", Alt: 'Angular' },
-    { Page: Threejs, name: 'Three js', class: 'SVG Threejs', SVG: "Threejs", Alt: 'Three js' },
+const Skill4: Type = [
+    { Page: Svelte, name: 'Svelte', class: 'SVG Svelte', SVG: "Svelte" },
+    { Page: React, name: 'React', class: 'SVG React', SVG: "React" },
+    { Page: Angular, name: 'Angular', class: 'SVG Angular', SVG: "Angular", },
+    { Page: Threejs, name: 'Three js', class: 'SVG Threejs', SVG: "Threejs" },
 ]
 
-const Skill5 = [
-    { Page: MySQL, name: 'MySQL', class: 'SVG MySQL', SVG: "MySQL", Alt: 'MySQL' }
+const Skill5: Type = [
+    { Page: MySQL, name: 'MySQL', class: 'SVG MySQL', SVG: "MySQL" }
 ]
 
-const Skill6 = [
-    { Page: Postman, name: 'Postman', class: 'SVG Postman', SVG: "Postman", Alt: 'Postman' }
+const Skill6: Type = [
+    { Page: Postman, name: 'Postman', class: 'SVG Postman', SVG: "Postman", }
 ]
 
-const Skill7 = [
-    { Page: VSCode, name: 'VSCode', class: 'SVG VSCode', SVG: 'VSCode', Alt: 'VS Code' },
-    { Page: GitHub, name: 'GitHub', class: 'SVG GitHub', SVG: 'GitHub', Alt: 'GitHub' },
-    { Page: ProjectIDX, name: 'Project IDX', class: 'SVG ProjectIDX', SVG: 'IDX', Alt: 'Project IDX' },
-    { Page: Eclipse, name: 'Eclipse', class: 'SVG Eclipse', SVG: 'Eclipse', Alt: 'Eclipse' },
-    { Page: IntelliJ, name: 'IntelliJ', class: 'SVG InteliJ', SVG: 'IntelliJ', Alt: 'InteliJ Idea' },
+const Skill7: Type = [
+    { Page: VSCode, name: 'VSCode', class: 'SVG VSCode', SVG: 'VSCode' },
+    { Page: GitHub, name: 'GitHub', class: 'SVG GitHub', SVG: 'GitHub' },
+    { Page: ProjectIDX, name: 'Project IDX', class: 'SVG ProjectIDX', SVG: 'IDX' },
+    { Page: Eclipse, name: 'Eclipse', class: 'SVG Eclipse', SVG: 'Eclipse' },
+    { Page: IntelliJ, name: 'IntelliJ', class: 'SVG InteliJ', SVG: 'IntelliJ' },
 ]
 
 export { Skill1, Skill2, Skill3, Skill4, Skill5, Skill6, Skill7 }
