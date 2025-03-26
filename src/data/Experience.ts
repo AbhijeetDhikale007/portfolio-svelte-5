90d6bbb9878541779e23a5db23eb772c280fa3d0
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-26 14:29:02 +0000

    Created: Experience Data

---
+++ src/data/Experience.ts
@@ -0,0 +1,16 @@
+type Cards = {
+    CardClass: string,
+    CertificateUrl: string,
+    imgUrl: string,
+    imgAlt: string,
+    Title: string,
+    Company: string,
+    Location: string,
+    Date: string,
+    Duration: string,
+}[]
+
+export let Experience: Cards = [
+    { CardClass: 'CardEdu Card1', CertificateUrl: 'Documents/Internship - Calanjiyam Consultancy.pdf', imgUrl: 'assets/Images/School.png', imgAlt: 'Calanjiyam Consultancies and Technologies', Title: 'Full stack Web application Development', Company: 'Calanjiyam Consultancies and Technologies', Location: 'Erode, Tamil Nadu - 638455', Date: '3 Jan 2025 - 5 Mar 2025', Duration: '8 Weeks'},
+    // { CardClass: 'CardEdu Card2', CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'assets/Images/MVP.png', imgAlt: 'MVP', Title: 'Industrial Training Institute', Company: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year' },
+]
