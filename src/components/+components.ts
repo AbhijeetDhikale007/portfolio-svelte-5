d8baae7e73e56f1b13e68891c3c2dfa4bf5e3c89
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-26 14:10:24 +0000

    Imported: Experience Component

--- src/components/+components.ts
+++ src/components/+components.ts
@@ -1,11 +1,12 @@
 import Ico from './Icons/Ico.svelte'
 import Icons from './Home/Icons.svelte'
 import Carousel from './Home/Carousel.svelte'
-import Heading from "./Heading.svelte";
-import EducationCard from "./Education/EducationCard.svelte";
-import CardProject from './Projects/CardProject.svelte';
-import CardDiv from "./Skills/CardDiv.svelte";
+import Heading from './Heading.svelte'
+import ExperienceCard from './Experience/ExperienceCard.svelte'
+import EducationCard from './Education/EducationCard.svelte'
+import CardProject from './Projects/CardProject.svelte'
+import CardDiv from './Skills/CardDiv.svelte'
 import { Card1, Card2, Card3, Card4, Card5, Card6, Card7 } from "./Skills/Cards";
-import Typewriter from "./Typewriter.svelte";
+import Typewriter from './Typewriter.svelte'
 
-export { Ico, Icons, Carousel, Heading, EducationCard, CardProject, CardDiv, Card1, Card2, Card3, Card4, Card5, Card6, Card7, Typewriter}
+export { Ico, Icons, Carousel, Heading, ExperienceCard, EducationCard, CardProject, CardDiv, Card1, Card2, Card3, Card4, Card5, Card6, Card7, Typewriter}
