dc9c2768c7a8cb943eb27ac20a88d8d14469385e
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-26 14:28:39 +0000

    Imported: Experience Page

--- src/routes/+pages.ts
+++ src/routes/+pages.ts
@@ -3,7 +3,8 @@ import Home from './Home/Home.svelte'
 import SkillsPage from './Skills/SkillsPage.svelte'
 import Skills from './Skills/Skills.svelte'
 import Projects from './Projects/Projects.svelte'
+import Experience from './Experience/Experience.svelte'
 import Education from './Education/Education.svelte'
 import Resume from './Resume/Resume.svelte'
 
-export { Header, Home, SkillsPage, Skills, Projects, Education, Resume }
+export { Header, Home, SkillsPage, Skills, Projects, Experience, Education, Resume }
