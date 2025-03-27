fd80f87d0a5b1693168d43713292baa594f71078
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-27 16:17:17 +0000

    Implemented: Font Arsenal SC

--- src/components/Heading.svelte
+++ src/components/Heading.svelte
@@ -2,10 +2,14 @@
     export let HeadingTitle: string
 </script>
 
-<h1 class='Heading'>{HeadingTitle}</h1>
+<h1 class='Heading Arsenal-SC'>{HeadingTitle}</h1>
 
 <style lang='scss'>
     .Heading {
         @apply text-center mt-21 mb-3 text-15 font-extrabold;
+    }
+
+    .Arsenal-SC {
+        font-family: 'Arsenal SC';
     }
 </style>
